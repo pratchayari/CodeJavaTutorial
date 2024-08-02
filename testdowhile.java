bc2904370b22bb41bdd93c6623e61257d82b11d7
pratchayari <aokisawa@gmail.com>
2024-08-02 08:59:03 +0700

    Create testdowhile.java

---
+++ testdowhile.java
@@ -0,0 +1,18 @@
+public class testdowhile {
+    public static void main(String[] args){
+        int i;
+        for(i = 1; i <= 10; i++){
+            System.out.println("i in for :" +i);
+        }
+        int iwhile = 1;
+        while(iwhile <= 10){
+            System.out.println("iwhile in while : " +iwhile);
+            iwhile++;
+        }
+
+        int counter = 1;
+        do{
+            System.out.println("counter in dowhile" +counter);
+        }while(++counter <= 10);
+    }
+}
