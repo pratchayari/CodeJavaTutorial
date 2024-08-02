ffc63a50097eb7570fa84b77cdf7c2aa77ba0be8
pratchayari <aokisawa@gmail.com>
2024-08-02 08:58:30 +0700

    Create Payroll.java

---
+++ Payroll.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Payroll {
+    public static void main(String[] args) {
+        /*
+         1.nextByte()ชนิดข้อมูลแบบ Byte
+         2.nextDouble() ชนิดข้อมูลแบบ Double
+         3.nextFloat() ชนิดข้อมูลแบบ Float
+         4.nextInt()ชนิดข้อมูลแบบ Int
+         5.nextLine()ชนิดข้อมูลแบบ String
+         6.nextLong()ชนิดข้อมูลแบบ Long
+         7.nextShort()ชนิดข้อมูลแบบ Short
+         */
+        String name;
+        int hours;
+        double payRate;
+        double grossPay;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        name = keyboard.nextLine();
+        System.out.print("How many hours did you work this week? ");
+        hours = keyboard.nextInt();
+        System.out.print("What is your hourly pay rate? ");
+        payRate = keyboard.nextDouble();
+        grossPay = hours * payRate;
+        System.out.println("Hello "+name);
+        System.out.println("Your gross pay is "+grossPay);
+    }
+}
