ee9ae542abb9ab31974925504009715bcdcdcab0
pratchayari <aokisawa@gmail.com>
2024-08-02 08:59:17 +0700

    Create WhileLoop.java

---
+++ WhileLoop.java
@@ -0,0 +1,9 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int iWhile = 1;
+        while (iWhile <= 5) {
+            System.out.println("hello");
+            iWhile++ ;
+        }
+    }
+}
