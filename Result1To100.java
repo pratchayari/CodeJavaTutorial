45ebc35b09d738b086eeefacf8970a7e0b083d68
pratchayari <aokisawa@gmail.com>
2024-08-02 08:58:32 +0700

    Create Result1To100.java

---
+++ Result1To100.java
@@ -0,0 +1,11 @@
+public class Result1To100 {
+    public static void main(String[] args) {
+        int i = 1, sum = 0 ;
+        while (i <= 100) {
+            sum +=i ;
+            System.out.println("i : " +i+ " sum : " +sum);
+            i++ ;
+        }
+    System.out.println("Result 1 To 100 : "+sum);
+    }
+}
